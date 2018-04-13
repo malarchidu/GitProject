dbf2c507f59db886352de3df8e4d4ace4676e918
malar <malar@LAPTOP-00QR25KJ>
2018-04-13 09:52:12 -0700

    Added new code

--- GitProject/src/GitDemo1.java
+++ GitProject/src/GitDemo1.java
@@ -4,6 +4,7 @@ public class GitDemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello Team!");
+       System.out.println("Hello usa");
 	}
 
 }
